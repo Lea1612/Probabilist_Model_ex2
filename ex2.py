c2e5d10a089096eefd57506645fd3bd6d92e32b7
Lea <lilouset@gmail.com>
2020-11-30 22:24:56 +0200

    'fixing'

--- ex2.py
+++ ex2.py
@@ -1,5 +1,6 @@
 import sys
 from collections import Counter
+import math
 
 
 vocabulary_size = 300000
@@ -17,6 +18,12 @@ def string_words(file_name):
                     words_sequence.append(word)
 
     return words_sequence
+
+def write_output(output_filename, output):
+    with open(output_filename,"w") as f:
+        for i, row in enumerate(output):
+            f.write(f"#Output{i}\t{row}\n")
+    print("\n".join([str(i) for i in output]))
 
 
 class LidstoneModel(object):
@@ -37,9 +44,23 @@ class LidstoneModel(object):
         return float(word_count + lambda_var) / \
                (self.training_set_size + lambda_var * vocabulary_size)
 
+    # def calculate_perplexity(self, lambda_var=0.01):
+    #     sigma = sum([self.calculate_prob_lambda(w, lambda_var) for w in self.validation])
+    #     return 1 / self.validation_set_size * sigma
+
     def calculate_perplexity(self, lambda_var=0.01):
-        sigma = sum([self.calculate_prob_lambda(w, lambda_var) for w in self.validation])
-        return 1 / self.validation_set_size * sigma
+        sigma = sum([math.log2(self.calculate_prob_lambda(w, lambda_var)) for w in self.validation])
+        return 2**((-1 / self.validation_set_size) * sigma)
+
+    def best_lambda(self):
+        perplexity_values = []
+        
+        for lamda in range(1,201):
+            perplexity_values.append((self.calculate_perplexity(lambda_var= lamda / 100), lamda / 100))
+
+        min_preplexity, min_lambda = min(perplexity_values)
+
+        return min_preplexity, min_lambda
 
 
 def main():
@@ -64,7 +85,7 @@ def main():
 
     output[7] = len(train_words) 
 
-### Lidstone model training ###
+### 3. Lidstone model training ###
 
     threshold = round(0.9 * len(train_words))
     training_set = train_words[:threshold]  # The first 90% to train
@@ -75,21 +96,25 @@ def main():
     output[9] = lidstone_model.training_set_size       # Nb of events in training set
     output[10] = lidstone_model.different_events_counter_train # Nb of different events in train
     output[11] = lidstone_model.training_event_occuration[input_word]  # Nb of times the input_word occurs in train
-    output[12] = lidstone_model.calculate_prob(input_word)
-    output[13] = lidstone_model.calculate_prob()
-    output[14] = lidstone_model.calculate_prob_lambda(word=input_word)
-    output[15] = lidstone_model.calculate_prob_lambda()
+    output[12] = lidstone_model.calculate_prob(input_word) # MLE train
+    output[13] = lidstone_model.calculate_prob() #MLE on unseen words. word = None 
+    output[14] = lidstone_model.calculate_prob_lambda(word=input_word) # Proba Lidstone with lambda = 0.1
+    output[15] = lidstone_model.calculate_prob_lambda() # On unseen words. word = None
     lambda_var = [0.01, 0.1, 1.0]
-    output[16] = lidstone_model.calculate_perplexity(lambda_var[0])
-    output[17] = lidstone_model.calculate_perplexity(lambda_var[1])
-    output[18] = lidstone_model.calculate_perplexity(lambda_var[2])
-    output[20] = min(output[16:19])
-    output[19] = lambda_var[output[16:19].index(output[20])]
+    output[16] = lidstone_model.calculate_perplexity(lambda_var[0]) # Perplexity on valid. lambda = 0.01
+    output[17] = lidstone_model.calculate_perplexity(lambda_var[1]) # lambda = 0.1
+    output[18] = lidstone_model.calculate_perplexity(lambda_var[2]) # lambda = 1
+    # output[20] = min(output[16:19]) # minimized perplexity on valid
+    # output[19] = lambda_var[output[16:19].index(output[20])] # lambda that minimizes perplexity on valid
+    output[19] = lidstone_model.best_lambda()[1]
+    output[20] = lidstone_model.best_lambda()[0]
+
+    ### 4.  Held out training ###
+
     # output[21] =
-    with open("output.txt","w") as f:
-        for i, row in enumerate(output):
-            f.write(f"#Output{i}\t{row}\n")
-    print("\n".join([str(i) for i in output]))
+
+    write_output(output_filename, output)
+
 
 
 if __name__ == "__main__":
